d6247b2588fc4e691f6db4bed72db096b3ad4914
Anshul Tamrakar <anshultamrakar487@gmail.com>
2023-06-17 20:39:38 +0530

    chnages

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 import {IoMdArrowRoundBack} from "react-icons/io"
 import { Link } from 'react-router-dom'
 
 const Search = () => {
     const [search , setSearch] = useState("")
+
+    const handleSearch = (e) => {
+      setSearch(e.target.value)
+    }
   return (
     <div className='Search'>
         <Link to = "/"> <IoMdArrowRoundBack/></Link>
-       <input type = "text" placeholder='Search your book ' value = {search} onChange={(e) => console.log(e.target.value)} />
+       <input type = "text" placeholder='Search your book ' value = {search} onChange={handleSearch} />
     </div>
   )
 }
